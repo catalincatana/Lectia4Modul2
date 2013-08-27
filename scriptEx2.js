977658650711c1b5be6433abf2e65cb9427be6f7
Mardaloescu <mardaloescu@mardaloescu-PC.(none)>
2013-08-27 17:44:39 +0300

    ceva

--- scriptEx2.js
+++ scriptEx2.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-	var cnv = document.getElementById("myCanvas");
-	var jg = new jsGraphics(cnv);
-	jg.drawLine(5, -20, 30, 20); // co-ordinates related to "myCanvas"
-	jg.paint();
+	var c11 = document.getElementById("11");
+	var jg11 = new jsGraphics(c11);
+	jg11.drawLine(5, -20, 33, 20); // co-ordinates related to "myCanvas"
+	jg11.paint();
+	
+	var c12 = document.getElementById("12");
+	var jg12 = new jsGraphics(c12);
+	jg12.drawLine(30, -20, 30, 20); // co-ordinates related to "myCanvas"
+	jg12.paint();
+
+	var c21 = document.getElementById("21");
+	var jg21 = new jsGraphics(c21);
+	jg21.drawLine(5, -50, 30, -8); // co-ordinates related to "myCanvas"
+	jg21.paint();
+
+	var c22 = document.getElementById("22");
+	var jg22 = new jsGraphics(c22);
+	jg22.drawLine(27, -50, 27, -8); // co-ordinates related to "myCanvas"
+	jg22.paint();
+
 });
