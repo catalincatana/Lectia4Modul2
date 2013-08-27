edb4d3bc7aafcd212a56058e926c8e649a4d3abb
Mardaloescu <mardaloescu@mardaloescu-PC.(none)>
2013-08-27 10:59:22 +0300

    modificari index

--- script.js
+++ script.js
@@ -17,75 +17,95 @@ $(document).ready(function() {
 		$("#wrong").show("slow");
 	}
 	
+//	$("#diva").hide();
+//	$("#divb").hide();
+//	$("#divc").hide();
+//	$("#divd").hide();
+	//Funcie ce afiseaza div-urile corespunzatoare butoanelor a, b, c, d
+	var clickOnButton = function() {
+		$("#btna").click(function() {
+			$("#diva").show("fast");
+		});
+		$("#btnb").click(function() {
+			$("#divb").show("fast");
+		});
+		$("#btnc").click(function() {
+			$("#divc").show("fast");
+		});
+		$("#btnd").click(function() {
+			$("#divd").show("fast");
+		});
+	};
+	clickOnButton();
 	//functia ce construieste tabelul
 	var createTable = function () {
-		$("tbody").append("<tr>");	//Linia 1
-			$("tbody").append("<td> 1 </td>");
-			$("tbody").append("<td> 2 </td>");
-			$("tbody").append("<td><input id='input13' maxlength='1' size='1'></td>");
-			$("tbody").append("<td><input id='input14' maxlength='1' size='1'></td>");
-			$("tbody").append("<td><input id='input15' maxlength='2' size='1'></td>");
-			$("tbody").append("<td><input id='input16' maxlength='2' size='1'></td>");
-			$("tbody").append("<td><input id='input17' maxlength='2' size='1'></td>");
-			$("tbody").append("<td><input id='input18' maxlength='2' size='1'></td>");
-		$("tbody").append("</tr>");
-		$("tbody").append("<tr>");	//Linia 2
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input21' maxlength='1' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input22' maxlength='1' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input23' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input24' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input25' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input26' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input27' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td>2<sup><input class='roundInput' id='input28' maxlength='2' size='1'></sup></td>");
-		$("tbody").append("</tr>");
-		$("tbody").append("<tr>");	//Linia 3
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td class='greenCell'>2<sup><input class='roundInput' id='input33' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-		$("tbody").append("</tr>");
+		$("#tbody2").append("<tr>");	//Linia 1
+			$("#tbody2").append("<td> 1 </td>");
+			$("#tbody2").append("<td> 2 </td>");
+			$("#tbody2").append("<td><input id='input13' maxlength='1' size='1'></td>");
+			$("#tbody2").append("<td><input id='input14' maxlength='1' size='1'></td>");
+			$("#tbody2").append("<td><input id='input15' maxlength='2' size='1'></td>");
+			$("#tbody2").append("<td><input id='input16' maxlength='2' size='1'></td>");
+			$("#tbody2").append("<td><input id='input17' maxlength='2' size='1'></td>");
+			$("#tbody2").append("<td><input id='input18' maxlength='2' size='1'></td>");
+		$("#tbody2").append("</tr>");
+		$("#tbody2").append("<tr>");	//Linia 2
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input21' maxlength='1' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input22' maxlength='1' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input23' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input24' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input25' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input26' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input27' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td>2<sup><input class='roundInput' id='input28' maxlength='2' size='1'></sup></td>");
+		$("#tbody2").append("</tr>");
+		$("#tbody2").append("<tr>");	//Linia 3
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td class='greenCell'>2<sup><input class='roundInput' id='input33' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+		$("#tbody2").append("</tr>");
 
 		for(var i = 0; i < 2; i++) { 	//Liniile 4 & 5
-			$("tbody").append("<tr>");
+			$("#tbody2").append("<tr>");
 			for(var j = 0; j < 8; j++) {
-				$("tbody").append("<td></td>");
+				$("#tbody2").append("<td></td>");
 			}
-			$("tbody").append("</tr>");
+			$("#tbody2").append("</tr>");
 		}
 		
-		$("tbody").append("<tr>");	//Linia 6
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td class='greenCell'>2<sup><input class='roundInput' id='input63' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td class='greenCell'>2<sup><input class='roundInput' id='input67' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td></td>");
-		$("tbody").append("</tr>");
+		$("#tbody2").append("<tr>");	//Linia 6
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td class='greenCell'>2<sup><input class='roundInput' id='input63' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td class='greenCell'>2<sup><input class='roundInput' id='input67' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td></td>");
+		$("#tbody2").append("</tr>");
 
-		$("tbody").append("<tr>");	//Linia 7
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td class='greenCell'>2<sup><input class='roundInput' id='input75' maxlength='2' size='1'></sup></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-			$("tbody").append("<td></td>");
-		$("tbody").append("</tr>");
+		$("#tbody2").append("<tr>");	//Linia 7
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td class='greenCell'>2<sup><input class='roundInput' id='input75' maxlength='2' size='1'></sup></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+			$("#tbody2").append("<td></td>");
+		$("#tbody2").append("</tr>");
 		
-		$("tbody").append("<tr>");		//Linia 8
+		$("#tbody2").append("<tr>");		//Linia 8
 			for(var j = 0; j < 7; j++) {
-				$("tbody").append("<td></td>");
+				$("#tbody2").append("<td></td>");
 			}
-			$("tbody").append("<td class='greenCell'>2<sup><input class='roundInput' id='input88' maxlength='2' size='1'></sup></td>");
-		$("tbody").append("</tr>");
+			$("#tbody2").append("<td class='greenCell'>2<sup><input class='roundInput' id='input88' maxlength='2' size='1'></sup></td>");
+		$("#tbody2").append("</tr>");
 	};
 	
 	createTable();
