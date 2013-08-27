4a150444c135ebe28eacc63c285ae4c77bf3aeca
catalincatana <catalin.catana3@gmail.com>
2013-08-27 14:49:26 +0300

    last1

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ $(document).ready(function() {
 			if(noPatrate == 64) {
 				$("#check").show();
 				console.log('Este corect');
-			} else if(!isNaN(noPatrate)) {
+			}
+			else if(noPatrate == 6)
+			{
+				$("#check").hide();
+				$("#wrong").hide();
+			}
+			else if(!isNaN(noPatrate)) {
 				$("#check").hide();
 				$("#wrong").show("slow");
 				$("#wrong").hide("slow");
@@ -165,6 +171,7 @@ $(document).ready(function() {
 				$("#check1 > img").hide("slow");
 				}
 			else{
+				$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 				}
@@ -183,6 +190,7 @@ $(document).ready(function() {
 				$("#check1 > img").hide("slow");
 				}
 			else{
+				$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 				}
@@ -205,6 +213,7 @@ $(document).ready(function() {
 				$("#wrong1 > img").hide("slow");
 				}
 			else{
+				$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 				}
@@ -227,6 +236,7 @@ $(document).ready(function() {
 				$("#wrong1 > img").hide("slow");
 				}
 			else{
+				$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 				}
@@ -249,6 +259,7 @@ $(document).ready(function() {
 				$("#wrong1 > img").hide("slow");
 				}
 			else{
+				$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 				}
@@ -261,7 +272,14 @@ $(document).ready(function() {
 			
 			if(!(isNaN(i18)))
 			{
-			if(i18 == 128 ){
+			if(((i18 == 128 && i17==64)&& (i16==32 && i15 ==16))&&(i14==8 && i13==4))
+					
+						{
+				$("#check1 > img").show("slow");
+				//$("#check1 > img").hide("slow");
+				}
+			else if(i18 == 128)
+						{
 				$("#check1 > img").show("slow");
 				$("#check1 > img").hide("slow");
 				}
@@ -277,12 +295,326 @@ $(document).ready(function() {
 			}
 			else
 			{
+			$("#check1 > img").hide("slow");
 				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
 			}
 			}
 		}).keydown();
+	}
+		// checks pt a doua linie
+		var i21,i22,i23,i24,i25,i26,i27,i28;
+	if(isNaN(i21)||isNaN(i22)||isNaN(i23)||isNaN(i24)||isNaN(i25)||isNaN(i26)||isNaN(i27)||isNaN(i28)) {
 		
+		$("#input21").keyup(function() {
+			i21 = $("#input21").val();
+			i21 = parseInt(i21, 10);
+			
+			if(!(isNaN(i21)))
+			{
+			if(i21 == 8 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input22").keyup(function() {
+			
+			i22 = $("#input22").val();
+			i22 = parseInt(i22, 10);
+			
+			if(!(isNaN(i22)))
+			{
+			if(i22 == 9 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input23").keyup(function() {
+			
+			i23 = $("#input23").val();
+			i23 = parseInt(i23, 10);
+			
+			if(!(isNaN(i23)))
+			{
+			if(i23 == 10 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i23 ==1)
+				{
+				$("#wrong2 > img").hide("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input24").keyup(function() {
+			
+			i24 = $("#input24").val();
+			i24 = parseInt(i24, 10);
+			
+			if(!(isNaN(i24)))
+			{
+			if(i24 == 11 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i24 ==1)
+				{
+				$("#wrong2 > img").hide("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input25").keyup(function() {
+			
+			i25 = $("#input25").val();
+			i25 = parseInt(i25, 10);
+			
+			if(!(isNaN(i25)))
+			{
+			if(i25 == 12 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i25 ==1)
+				{
+				$("#wrong2 > img").hide("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input26").keyup(function() {
+			
+			i26 = $("#input26").val();
+			i26 = parseInt(i26, 10);
+			
+			if(!(isNaN(i26)))
+			{
+			if(i26 == 13 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i26 ==1)
+				{
+				$("#wrong2 > img").hide("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input27").keyup(function() {
+			
+			i27 = $("#input27").val();
+			i27 = parseInt(i27, 10);
+			
+			if(!(isNaN(i27)))
+			{
+			if(i27 == 14 ){
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i27 ==1)
+				{
+				$("#wrong2 > img").hide("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			else{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input28").keyup(function() {
+			
+			i28 = $("#input28").val();
+			i28 = parseInt(i28, 10);
+			
+			if(!(isNaN(i28)))
+			{
+			if((((i28 == 15 && i27==14)&& (i26==13 && i25 ==12))&&(i24==11 && i23==10))&&(i22==9 && i21==8))
+					
+						{
+				$("#check2 > img").show("slow");
+				//$("#check1 > img").hide("slow");
+				}
+			else if(i28 == 15)
+						{
+				$("#check2 > img").show("slow");
+				$("#check2 > img").hide("slow");
+				}
+			else if(i28 ==1)
+				{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			else
+			{
+				$("#check2 > img").hide("slow");
+				$("#wrong2 > img").show("slow");
+				$("#wrong2 > img").hide("slow");
+			}
+			}
+		}).keydown();
+		// checks pt elementele verzi (a 3-a linie)
+		// checks pt a doua linie
+		var i33,i63,i67,i75,i88;
+	if(isNaN(i33)||isNaN(i63)||isNaN(i67)||isNaN(i75)||isNaN(i8)) {
+		
+		$("#input33").keyup(function() {
+			i33 = $("#input33").val();
+			i33 = parseInt(i33, 10);
+			
+			if(!(isNaN(i33)))
+			{
+			if(i33 == 18 ){
+				$("#check3 > img").show("slow");
+				$("#check3 > img").hide("slow");
+				}
+			else if(i33 ==1)
+				{
+				$("#wrong3 > img").hide("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			else{
+				$("#check3 > img").hide("slow");
+				$("#wrong3 > img").show("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input63").keyup(function() {
+			i63 = $("#input63").val();
+			i63 = parseInt(i63, 10);
+			
+			if(!(isNaN(i63)))
+			{
+			if(i63 == 42 ){
+				$("#check3 > img").show("slow");
+				$("#check3 > img").hide("slow");
+				}
+			else if(i63 ==4)
+				{
+				$("#wrong3 > img").hide("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			else{
+				$("#check3 > img").hide("slow");
+				$("#wrong3 > img").show("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		
+		$("#input67").keyup(function() {
+			i67 = $("#input67").val();
+			i67 = parseInt(i67, 10);
+			
+			if(!(isNaN(i67)))
+			{
+			if(i67 == 46 ){
+				$("#check3 > img").show("slow");
+				$("#check3 > img").hide("slow");
+				}
+			else if(i67 ==4)
+				{
+				$("#wrong3 > img").hide("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			else{
+				$("#check3 > img").hide("slow");
+				$("#wrong3 > img").show("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input75").keyup(function() {
+			i75 = $("#input75").val();
+			i75 = parseInt(i75, 10);
+			
+			if(!(isNaN(i75)))
+			{
+			if(i75 == 52 ){
+				$("#check3 > img").show("slow");
+				$("#check3 > img").hide("slow");
+				}
+			else if(i75 ==5)
+				{
+				$("#wrong3 > img").hide("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			else{
+				$("#check3 > img").hide("slow");
+				$("#wrong3 > img").show("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input88").keyup(function() {
+			
+			i88 = $("#input88").val();
+			i88 = parseInt(i88, 10);
+			
+			if(!(isNaN(i88)))
+			{
+			if(((i88 == 63 && i75==52)&& (i67==46 && i63 ==42))&& i33 == 18)
+					
+						{
+				$("#check3 > img").show("slow");
+				//$("#check1 > img").hide("slow");
+				}
+			else if(i88 == 63)
+						{
+				$("#check3 > img").show("slow");
+				$("#check3 > img").hide("slow");
+				}
+			else if(i88 ==6)
+				{
+				$("#wrong3 > img").hide("slow");
+				$("#wrong3 > img").hide("slow");
+				}
+			else
+			{
+				$("#check3 > img").hide("slow");
+				$("#wrong3 > img").show("slow");
+				$("#wrong3 > img").hide("slow");
+			}
+			}
+		}).keydown();
+		
+	}
 		$("img").hide();
+		$("#dWrong1").show();
 	}
 });
