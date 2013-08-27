4e3cc6babd1fb61dd95ebdf811d43155f75ab033
catalincatana <catalin.catana3@gmail.com>
2013-08-27 13:16:47 +0300

    1:16

--- script.js
+++ script.js
@@ -145,29 +145,141 @@ $(document).ready(function() {
 		$("#input13").keyup(function() {
 			i13 = $("#input13").val();
 			i13 = parseInt(i13, 10);
+			i14 = $("#input14").val();
+			i14 = parseInt(i14, 10);
+			i15 = $("#input15").val();
+			i15 = parseInt(i15, 10);
+			i16 = $("#input16").val();
+			i16 = parseInt(i16, 10);
+			i17 = $("#input17").val();
+			i17 = parseInt(i17, 10);
+			i18 = $("#input18").val();
+			i18 = parseInt(i18, 10);
 			console.log(i13);
-			if(i13 == 4 && i14 == 8) {
-				$("#check1 > img").show();
-				console.log('Este corect');
-			} else if(!(isNaN(i13)||isNaN(i13))) {
-				$("#check1 > img").hide();
-				$("#wrong1 > img" ).show("slow");
+			console.log(i14);
+			if(!(isNaN(i13)))
+			{
+			console.log(i13);
+			if(i13 == 4 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else{
+				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
-				console.log('Gresit');
+				}
 			}
 		}).keydown();
 		$("#input14").keyup(function() {
+			
 			i14 = $("#input14").val();
 			i14 = parseInt(i14, 10);
-			console.log(i14);
-			if(i13 == 4 && i14 == 8) {
-				$("#check1 > img").show();
-				console.log('Este corect');
-			} else if(!(isNaN(i13)||isNaN(i13))) {
-				$("#check1 > img").hide();
-				$("#wrong1 > img" ).show("slow");
+			
+			if(!(isNaN(i14)))
+			{
+			console.log(i13);
+			if(i14 == 8 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else{
+				$("#wrong1 > img").show("slow");
 				$("#wrong1 > img").hide("slow");
-				console.log('Gresit');
+				}
+			}
+		}).keydown();
+		$("#input15").keyup(function() {
+			
+			i15 = $("#input15").val();
+			i15 = parseInt(i15, 10);
+			
+			if(!(isNaN(i15)))
+			{
+			if(i15 == 16 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else if(i15 ==1)
+				{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			else{
+				$("#wrong1 > img").show("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input16").keyup(function() {
+			
+			i16 = $("#input16").val();
+			i16 = parseInt(i16, 10);
+			
+			if(!(isNaN(i16)))
+			{
+			if(i16 == 32 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else if(i16 ==3)
+				{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			else{
+				$("#wrong1 > img").show("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input17").keyup(function() {
+			
+			i17 = $("#input17").val();
+			i17 = parseInt(i17, 10);
+			
+			if(!(isNaN(i17)))
+			{
+			if(i17 == 64 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else if(i17 ==6)
+				{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			else{
+				$("#wrong1 > img").show("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			}
+		}).keydown();
+		$("#input18").keyup(function() {
+			
+			i18 = $("#input18").val();
+			i18 = parseInt(i18, 10);
+			
+			if(!(isNaN(i18)))
+			{
+			if(i18 == 128 ){
+				$("#check1 > img").show("slow");
+				$("#check1 > img").hide("slow");
+				}
+			else if(i18 ==1)
+				{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+				}
+			else if(i18 == 12)
+			{
+				$("#wrong1 > img").hide("slow");
+				$("#wrong1 > img").hide("slow");
+			}
+			else
+			{
+				$("#wrong1 > img").show("slow");
+				$("#wrong1 > img").hide("slow");
+			}
 			}
 		}).keydown();
 		
